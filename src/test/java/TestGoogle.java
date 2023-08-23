4e52d49f6b4ed7c3b32793213e9cdc25956d9e07
Elangovan <prince.elango94@gmail.com>
2023-08-23 11:28:11 +0530

    Initial commit

---
+++ src/test/java/TestGoogle.java
@@ -0,0 +1,53 @@
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+
+public class TestGoogle {
+    public TestLeaf testLeaf;
+    public OpenAmazon openAmazon;
+    public WebDriver driver;
+    public ExtentReports extentReports;
+    public ExtentTest extentTest;
+
+    public TestGoogle() {
+        testLeaf = new TestLeaf();
+        openAmazon = new OpenAmazon();
+    }
+
+    @BeforeSuite
+    public void driverInit() {
+        driver = new FirefoxDriver();
+    }
+    public void setUp(){
+        ExtentSparkReporter reporter = new ExtentSparkReporter("D:/IntelliiJ/Code/SeleniumMaven/TestNG/test-output/extent-report");
+        extentReports=new ExtentReports();
+        extentReports.attachReporter(reporter);
+        extentTest.warning("a");
+
+    }
+
+    @Test(priority = 0)
+    public void runGoogle() {
+        testLeaf.openGoogle(driver);
+    }
+
+    @Test(priority = 1)
+    public void runAmazon() {
+        openAmazon.openAmazon(driver);
+    }
+
+    @AfterSuite
+    public void driverEnd() {
+        driver.close();
+    }
+    public void tearDown(){
+        extentReports.flush();
+    }
+
+}
---
+++ src/test/java/TestGoogle1.java
@@ -0,0 +1,39 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+
+public class TestGoogle1 {
+    public TestLeaf testLeaf;
+    public OpenAmazon openAmazon;
+    public WebDriver driver;
+
+    public TestGoogle1() {
+        testLeaf = new TestLeaf();
+        openAmazon = new OpenAmazon();
+    }
+
+    @BeforeSuite
+    public void driverInit() {
+        driver = new FirefoxDriver();
+
+    }
+
+    @Test(priority = 0)
+    public void runGoogle() {
+        testLeaf.openGoogle(driver);
+    }
+
+    @Test(priority = 1)
+    public void runAmazon() {
+        openAmazon.openAmazon(driver);
+    }
+
+    @AfterSuite
+    public void driverEnd() {
+        driver.close();
+    }
+
+}
